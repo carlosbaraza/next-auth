c418a564aab227adb7034a1f896b8cf3f6ccd270
Carlos Baraza Haro <carlos.baraza@ieee.org>
2020-09-14 00:52:15 +0100

    Don't log basePath build

--- dist/server/index.js
+++ dist/server/index.js
@@ -50,7 +50,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 if (!process.env.NEXTAUTH_URL) {
-  _logger.default.warn('NEXTAUTH_URL', 'NEXTAUTH_URL environment variable not set');
+  _logger.default.warn("NEXTAUTH_URL", "NEXTAUTH_URL environment variable not set");
 }
 
 var _default = function () {
@@ -77,7 +77,6 @@ var _default = function () {
         if (userSuppliedOptions.baseUrl) baseUrl = userSuppliedOptions.baseUrl;
         var basePath = parsedUrl.basePath;
         if (userSuppliedOptions.basePath) basePath = userSuppliedOptions.basePath;
-        console.log("basePath", basePath);
         var adapter;
 
         if (userSuppliedOptions.adapter) {
@@ -86,37 +85,37 @@ var _default = function () {
           adapter = _adapters.default.Default(userSuppliedOptions.database);
         }
 
-        var secret = userSuppliedOptions.secret || (0, _crypto.createHash)('sha256').update(JSON.stringify(_objectSpread({
+        var secret = userSuppliedOptions.secret || (0, _crypto.createHash)("sha256").update(JSON.stringify(_objectSpread({
           baseUrl,
           basePath
-        }, userSuppliedOptions))).digest('hex');
-        var useSecureCookies = userSuppliedOptions.useSecureCookies || baseUrl.startsWith('https://');
-        var cookiePrefix = useSecureCookies ? '__Secure-' : '';
+        }, userSuppliedOptions))).digest("hex");
+        var useSecureCookies = userSuppliedOptions.useSecureCookies || baseUrl.startsWith("https://");
+        var cookiePrefix = useSecureCookies ? "__Secure-" : "";
 
         var cookies = _objectSpread({
           sessionToken: {
             name: "".concat(cookiePrefix, "next-auth.session-token"),
             options: {
               httpOnly: true,
-              sameSite: 'lax',
-              path: '/',
+              sameSite: "lax",
+              path: "/",
               secure: useSecureCookies
             }
           },
           callbackUrl: {
             name: "".concat(cookiePrefix, "next-auth.callback-url"),
             options: {
-              sameSite: 'lax',
-              path: '/',
+              sameSite: "lax",
+              path: "/",
               secure: useSecureCookies
             }
           },
           csrfToken: {
-            name: "".concat(useSecureCookies ? '__Host-' : '', "next-auth.csrf-token"),
+            name: "".concat(useSecureCookies ? "__Host-" : "", "next-auth.csrf-token"),
             options: {
               httpOnly: true,
-              sameSite: 'lax',
-              path: '/',
+              sameSite: "lax",
+              path: "/",
               secure: useSecureCookies
             }
           }
@@ -147,33 +146,33 @@ var _default = function () {
         var csrfTokenVerified = false;
 
         if (req.cookies[cookies.csrfToken.name]) {
-          var [csrfTokenValue, csrfTokenHash] = req.cookies[cookies.csrfToken.name].split('|');
+          var [csrfTokenValue, csrfTokenHash] = req.cookies[cookies.csrfToken.name].split("|");
 
-          if (csrfTokenHash === (0, _crypto.createHash)('sha256').update("".concat(csrfTokenValue).concat(secret)).digest('hex')) {
+          if (csrfTokenHash === (0, _crypto.createHash)("sha256").update("".concat(csrfTokenValue).concat(secret)).digest("hex")) {
             csrfToken = csrfTokenValue;
 
-            if (req.method === 'POST' && csrfToken === csrfTokenFromPost) {
+            if (req.method === "POST" && csrfToken === csrfTokenFromPost) {
               csrfTokenVerified = true;
             }
           }
         }
 
         if (!csrfToken) {
-          csrfToken = (0, _crypto.randomBytes)(32).toString('hex');
-          var newCsrfTokenCookie = "".concat(csrfToken, "|").concat((0, _crypto.createHash)('sha256').update("".concat(csrfToken).concat(secret)).digest('hex'));
+          csrfToken = (0, _crypto.randomBytes)(32).toString("hex");
+          var newCsrfTokenCookie = "".concat(csrfToken, "|").concat((0, _crypto.createHash)("sha256").update("".concat(csrfToken).concat(secret)).digest("hex"));
 
           _cookie.default.set(res, cookies.csrfToken.name, newCsrfTokenCookie, cookies.csrfToken.options);
         }
 
         var redirect = redirectUrl => {
-          var reponseAsJson = !!(req.body && req.body.json === 'true');
+          var reponseAsJson = !!(req.body && req.body.json === "true");
 
           if (reponseAsJson) {
             res.json({
               url: redirectUrl
             });
           } else {
-            res.status(302).setHeader('Location', redirectUrl);
+            res.status(302).setHeader("Location", redirectUrl);
             res.end();
           }
 
@@ -207,25 +206,25 @@ var _default = function () {
 
         options.callbackUrl = yield (0, _callbackUrlHandler.default)(req, res, options);
 
-        if (req.method === 'GET') {
+        if (req.method === "GET") {
           switch (action) {
-            case 'providers':
+            case "providers":
               (0, _providers2.default)(req, res, options, done);
               break;
 
-            case 'session':
+            case "session":
               (0, _session.default)(req, res, options, done);
               break;
 
-            case 'csrf':
+            case "csrf":
               res.json({
                 csrfToken
               });
               return done();
 
-            case 'signin':
+            case "signin":
               if (options.pages.signIn) {
-                var redirectUrl = "".concat(options.pages.signIn).concat(options.pages.signIn.includes('?') ? '&' : '?', "callbackUrl=").concat(options.callbackUrl);
+                var redirectUrl = "".concat(options.pages.signIn).concat(options.pages.signIn.includes("?") ? "&" : "?", "callbackUrl=").concat(options.callbackUrl);
 
                 if (req.query.error) {
                   redirectUrl = "".concat(redirectUrl, "&error=").concat(req.query.error);
@@ -234,7 +233,7 @@ var _default = function () {
                 return redirect(redirectUrl);
               }
 
-              _pages.default.render(req, res, 'signin', {
+              _pages.default.render(req, res, "signin", {
                 baseUrl,
                 basePath,
                 providers: Object.values(options.providers),
@@ -244,12 +243,12 @@ var _default = function () {
 
               break;
 
-            case 'signout':
+            case "signout":
               if (options.pages.signOut) {
-                return redirect("".concat(options.pages.signOut).concat(options.pages.signOut.includes('?') ? '&' : '?', "error=").concat(error));
+                return redirect("".concat(options.pages.signOut).concat(options.pages.signOut.includes("?") ? "&" : "?", "error=").concat(error));
               }
 
-              _pages.default.render(req, res, 'signout', {
+              _pages.default.render(req, res, "signout", {
                 baseUrl,
                 basePath,
                 csrfToken,
@@ -258,7 +257,7 @@ var _default = function () {
 
               break;
 
-            case 'callback':
+            case "callback":
               if (provider && options.providers[provider]) {
                 (0, _callback.default)(req, res, options, done);
               } else {
@@ -268,23 +267,23 @@ var _default = function () {
 
               break;
 
-            case 'verify-request':
+            case "verify-request":
               if (options.pages.verifyRequest) {
                 return redirect(options.pages.verifyRequest);
               }
 
-              _pages.default.render(req, res, 'verify-request', {
+              _pages.default.render(req, res, "verify-request", {
                 baseUrl
               }, done);
 
               break;
 
-            case 'error':
+            case "error":
               if (options.pages.error) {
-                return redirect("".concat(options.pages.error).concat(options.pages.error.includes('?') ? '&' : '?', "error=").concat(error));
+                return redirect("".concat(options.pages.error).concat(options.pages.error.includes("?") ? "&" : "?", "error=").concat(error));
               }
 
-              _pages.default.render(req, res, 'error', {
+              _pages.default.render(req, res, "error", {
                 baseUrl,
                 basePath,
                 error
@@ -296,9 +295,9 @@ var _default = function () {
               res.status(404).end();
               return done();
           }
-        } else if (req.method === 'POST') {
+        } else if (req.method === "POST") {
           switch (action) {
-            case 'signin':
+            case "signin":
               if (!csrfTokenVerified) {
                 return redirect("".concat(baseUrl).concat(basePath, "/signin?csrf=true"));
               }
@@ -309,7 +308,7 @@ var _default = function () {
 
               break;
 
-            case 'signout':
+            case "signout":
               if (!csrfTokenVerified) {
                 return redirect("".concat(baseUrl).concat(basePath, "/signout?csrf=true"));
               }
@@ -317,9 +316,9 @@ var _default = function () {
               (0, _signout.default)(req, res, options, done);
               break;
 
-            case 'callback':
+            case "callback":
               if (provider && options.providers[provider]) {
-                if (options.providers[provider].type === 'credentials' && !csrfTokenVerified) {
+                if (options.providers[provider].type === "credentials" && !csrfTokenVerified) {
                   return redirect("".concat(baseUrl).concat(basePath, "/signin?csrf=true"));
                 }
 
